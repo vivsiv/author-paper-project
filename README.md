2a4da2cf70ea1aed21373d30e7a233bfd8070969
Vivek <vivek.a.sivakumar@gmail.com>
2017-02-26 22:49:56 -0800

    move everything to pandas

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # author-paper-project
 CS 249 final project
+
+Feature List
+1. The Levenshtein distance between the names of the target author in Author.csv and PaperAuthor.csv.
+2. The Levenshtein distance between the affiliations of the target author in Author.csv and PaperAuthor.csv(*2).
+3. The ratio  of  matched  substring  between  the  names  of the target author in Author.csv and PaperAuthor.csv.
+4. The ratio of matched substring between the affiliations of the target author in Author.csv and PaperAuthor.csv.
+5. The ratio of matched substring between the abbreviated names of the target author inAuthor.csv and PaperAuthor.csv.
--- data-processing.py
+++ data-processing.py
@@ -1,7 +1,16 @@
 import sys
-import csv
-import os
+sys.path.append("/usr/local/lib/python2.7/site-packages")
+import numpy as np
+import pandas as pd
 import pickle
+import editdistance as edit
+
+
+string_replacements = {
+	"\\.*\\s+": " ",
+	"['\\-]": "",
+	"[^a-zA-Z ]": ""
+}
 
 def save_obj(obj, out_file_name):
     with open(out_file_name, 'wb') as f:
@@ -40,205 +49,75 @@ def standardize_name(name):
 
 	return [name1, name2, name3, name4, name5]
 
-def make_author_map(csv_reader, obj_dir):
-	author_map = {}
-
-	for read_row in csv_reader:
-		author_map[read_row['Id']] = (read_row['Name'], read_row['Affiliation'])
-
-	out_file_name = obj_dir + "author_map.pkl"
-	save_obj(author_map, out_file_name)
-
-	return author_map
-
-def make_paper_map(csv_reader, obj_dir):
-	paper_map = {}
-
-	for read_row in csv_reader:
-		paper_map[read_row['Id']] = read_row['Title']
-
-	out_file_name = obj_dir + "paper_map.pkl"
-	save_obj(paper_map, out_file_name)
-
-	return paper_map
-
-def make_author_paper_map(csv_reader, obj_dir):
-	author_paper_map = {}
-
-	for read_row in csv_reader:
-		author_paper_map[read_row['AuthorId'] + ":" + read_row['PaperId']] = (read_row['Name'], read_row['Affiliation'])
-
-	out_file_name = obj_dir + "author_paper_map.pkl"
-	save_obj(author_paper_map, out_file_name)
-
-	return author_paper_map
-
-def make_conference_map(csv_reader, obj_dir):
-	conference_map = {}
-
-	for read_row in csv_reader:
-		conference_map[read_row['Id']] = (read_row['ShortName'], read_row['FullName'], read_row['HomePage'])
-
-	out_file_name = obj_dir + "conference_map.pkl"
-	save_obj(conference_map, out_file_name)
-
-	return conference_map
-
-def make_journal_map(csv_reader, obj_dir):
-	journal_map = {}
-
-	for read_row in csv_reader:
-		journal_map[read_row['Id']] = (read_row['ShortName'], read_row['FullName'], read_row['HomePage'])
-
-	out_file_name = obj_dir + "journal_map.pkl"
-	save_obj(journal_map, out_file_name)
-
-	return journal_map 
-
-def transform_train_data(csv_reader, author_map, paper_map, author_paper_map):
-	out_file = open("Train_out.csv", 'w') 
-	csv_writer = csv.writer(out_file)
-	csv_writer.writerow(['author_id', 'paper_id', 
-		'a_name_1', 'a_name_2', 'a_name_3', 'a_name_4', 'a_name_5',
-		'a_affiliation',
-		'ap_name_1', 'ap_name_2', 'ap_name_3', 'ap_name_4', 'ap_name_5',
-		'ap_affiliation',
-		'paper_title', 'wrote_paper'])
-
-	for read_row in csv_reader:
-		author_id = read_row['AuthorId']
-
-		author_data = author_map.get(author_id, ("",""))
-
-		author_name = author_data[0]
-		std_author_names = standardize_name(author_name)
-
-		author_affiliation = author_data[1]
-		
-		confirmed_papers = read_row['ConfirmedPaperIds'].split(" ")
-		for paper_id in confirmed_papers:
-			paper_title = paper_map.get(paper_id, "")
-
-			author_paper_data = author_paper_map.get(author_id + ":" + paper_id, ("",""))
-
-			author_paper_name = author_paper_data[0]
-			std_author_paper_names = standardize_name(author_paper_name)
-
-			author_paper_affiliation = author_paper_data[1]
-
-			csv_writer.writerow([author_id, paper_id, 
-				std_author_names[0], std_author_names[1], std_author_names[2],std_author_names[3],std_author_names[4],
-				author_affiliation,
-				std_author_paper_names[0], std_author_paper_names[1], std_author_paper_names[2], std_author_paper_names[3], std_author_paper_names[4],
-				author_paper_affiliation,
-				paper_title, '1'])
-
-		deleted_papers = read_row['DeletedPaperIds'].split(" ")
-		for paper_id in deleted_papers:
-			paper_title = paper_map.get(paper_id, "")
-
-			author_paper_data = author_paper_map.get(author_id + ":" + paper_id, ("",""))
-
-			author_paper_name = author_paper_data[0]
-			std_author_paper_names = standardize_name(author_paper_name)
-
-			author_paper_affiliation = author_paper_data[1]
-
-			csv_writer.writerow([author_id, paper_id, 
-				std_author_names[0], std_author_names[1], std_author_names[2],std_author_names[3],std_author_names[4],
-				author_affiliation,
-				std_author_paper_names[0], std_author_paper_names[1], std_author_paper_names[2], std_author_paper_names[3], std_author_paper_names[4],
-				author_paper_affiliation,
-				paper_title, '0'])
-
 def main():
-	if len(sys.argv) < 3:
-		print "Usage python data-processing.py <data dir> <obj_dir> <Train.csv>..."
-		exit(1)
+	train_data = "./data/Train.csv"
+	print "Reading Train.csv from {0}".format(train_data)
+	train_df = pd.read_csv(train_data)
+
+	train_conf = pd.DataFrame(train_df.ConfirmedPaperIds.str.split(" ").tolist(), index=train_df.AuthorId).stack()
+	train_conf = train_conf.reset_index()[['AuthorId', 0]]
+	train_conf.columns = ["author_id", "paper_id"]
+	train_conf["wrote_paper"] = 1
+
+	train_deleted = pd.DataFrame(train_df.DeletedPaperIds.str.split(" ").tolist(), index=train_df.AuthorId).stack()
+	train_deleted = train_deleted.reset_index()[['AuthorId', 0]]
+	train_deleted.columns = ["author_id", "paper_id"]
+	train_deleted["wrote_paper"] = 0
+
+	train_mod = pd.concat([train_conf, train_deleted])
+	train_mod = train_mod["paper_id"].astype(int)
+
+	author_data = "./data/Author.csv"
+	print "Reading Author.csv from {0}".format(author_data)
+	author_df = pd.read_csv(author_data)
+	author_df = author_df.fillna("")
+	author_df["NameClean"] = author_df["Name"].replace(string_replacements, regex=True)
+	author_df["NameClean"] = author_df["NameClean"].str.lower()
+	author_df["AffiliationClean"] = author_df["Affiliation"].replace(string_replacements, regex=True)
+	author_df["AffiliationClean"] = author_df["AffiliationClean"].str.lower()
+	author_df.columns = ["author_id", "author_name", "author_affiliation", "author_name_clean", "author_affiliation_clean"]
 
 	
-	data_dir = sys.argv[1]
-	obj_dir = sys.argv[2]
-	
-	author_map = None
-	paper_map = None
-	conference_map = None
-	journal_map = None
-	author_paper_map = None
+	paper_data = "./data/Paper.csv"
+	print "Reading Paper.csv from {0}".format(paper_data)
+	paper_df = pd.read_csv(paper_data)
+	paper_df.columns = ['paper_id', 'paper_title', 'paper_year', 'conference_id', 'journal_id', 'paper_keyword']
 
-	print "Reading objects from: {0}".format(data_dir)
-	for file_name in os.listdir(obj_dir):
-		path_name = obj_dir + file_name
-		in_file = open(path_name)
-
-		if in_file == None:
-			print "Unable to open file: {0}".format(file_name)
-			exit(1)
-
-		if file_name == "author_map.pkl":
-			print "Loading author_map..."
-			author_map = load_obj(path_name)
-		elif file_name == "paper_map.pkl":
-			print "Loading paper_map..."
-			paper_map = load_obj(path_name)
-		elif file_name == "author_paper_map.pkl":
-			print "Loading author_paper_map..."
-			author_paper_map = load_obj(path_name)
-		else:
-			print "Not using object {0}".format(file_name)
+	paper_author_data = "./data/PaperAuthor.csv"
+	print "Reading PaperAuthor.csv from {0}".format(paper_author_data)
+	paper_author_df = pd.read_csv(paper_author_data)
+	paper_author_df = paper_author_df.fillna("")
+	paper_author_df["NameClean"] = paper_author_df["Name"].replace(string_replacements, regex=True)
+	paper_author_df["NameClean"] = paper_author_df["NameClean"].str.lower()
+	paper_author_df["AffiliationClean"] = paper_author_df["Affiliation"].replace(string_replacements, regex=True)
+	paper_author_df["AffiliationClean"] = paper_author_df["AffiliationClean"].str.lower()
+	paper_author_df.columns = ["paper_id", "author_id", "paper_author_name", "paper_author_affiliation", "paper_author_name_clean", "paper_author_affiliation_clean"]
 
 
-	print "Reading data from: {0}".format(data_dir)
-	for file_name in os.listdir(data_dir):
-		print "Reading file: {0}".format(file_name)
+	#Basic Name Edit Distance
+	author_name_df = author_df[['author_id', 'author_name', 'author_name_clean']]
+	paper_author_name_df = paper_author_df[['paper_id', 'author_id', 'paper_author_name', 'paper_author_name_clean']]
+	name_join = pd.merge(author_name_df, paper_author_name_df, how="left", on="author_id")
 
-		path_name = data_dir + file_name
-		in_file = open(path_name)
-
-		if in_file == None:
-			print "Unable to open file: {0}".format(in_file)
-			exit(1)
-
-		if file_name == "Author.csv":
-			if author_map == None:
-				print "Making author_map... saving to {0}".format(obj_dir)
-				author_map = make_author_map(csv.DictReader(in_file), obj_dir)
-			else :
-				print "Already loaded author_map from obj"
-		elif file_name == "Paper.csv":
-			if paper_map == None:
-				print "Making paper_map... saving to {0}".format(obj_dir)
-				paper_map = make_paper_map(csv.DictReader(in_file), obj_dir)
-			else :
-				print "Already loaded paper_map from obj"
-		elif file_name == "Conference.csv":
-			if conference_map == None:
-				print "Making conference_map... saving to {0}".format(obj_dir)
-				conference_map = make_conference_map(csv.DictReader(in_file), obj_dir)
-			else :
-				print "Already loaded conference_map from obj"
-		elif file_name == "Journal.csv":
-			if journal_map == None:
-				print "Making journal_map... saving to {0}".format(obj_dir)
-				journal_map = make_journal_map(csv.DictReader(in_file), obj_dir)
-			else :
-				print "Already loaded journal_map from obj"
-		elif file_name == "PaperAuthor.csv":
-			if author_paper_map == None:
-				print "Making author_paper_map... saving to {0}".format(obj_dir)
-				author_paper_map = make_author_paper_map(csv.DictReader(in_file), obj_dir)
-			else :
-				print "Already loaded author_paper_map from obj"	
-		else:
-			print "Not using {0}".format(file_name)
+	name_join['edit_dist'] = name_join.apply(lambda row: ed.eval(row['author_name_clean'], row['paper_author_name_clean']), axis=1)
+	name_join_train = name_join[['author_id', 'paper_id', 'edit_distance']]
+	train_mod = pd.merge(train_mod, name_join_train, how="left", on=['author_id', 'paper_id'])
 
 
-		if len(sys.argv) == 4:
-			train_file = open(sys.argv[3])
-			if train_file == None:
-				print "Unable to open file: {0}".format(file_name)
-				exit(1)
-			print "Transforming {0}".format(train_file)
-			transform_train_data(csv.DictReader(train_file), author_map, paper_map, author_paper_map)
+	#author_affiliation_df = author_df[['author_id', 'author_name', 'author_name_clean']]
+
+	#Generating author year stats
+	paper_join = pd.merge(paper_df, paper_author_df, how="left", on="paper_id")
+
+	author_years = paper_join.groupby(['author_Id'], sort=False)['Year']
+
+	ays_min = author_years.min().rename("min_pub_year")
+	ays_max = author_years.max().rename("max_pub_year")
+	ays_mean = author_years.mean().rename("mean_pub_year")
+
+	author_year_stats = pd.concat([ays_min, ays_max, ays_mean], axis=1)
+	author_year_stats['author_id'] = author_year_stats.index
+
+	author_df = pd.merge(author_df, author_year_stats, how="left", on="Id")
 
 if __name__ == "__main__": main()
